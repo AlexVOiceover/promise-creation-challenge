4170edd0bbcd5a5b0295a0d48b73f8d19cea7e4f
AlexVOiceover <alex@alexvoiceover.com>
2024-06-17 11:37:12 +0100

    Challenge02

--- challenge/read-file.js
+++ challenge/read-file.js
@@ -1,7 +1,17 @@
-const fs = require("node:fs");
+const fs = require('node:fs')
 
 function readFilePromise(filePath) {
-  //
+	const promise = new Promise((resolve, reject) => {
+		fs.readFile(filePath, 'utf8', (error, data) => {
+			if (error) {
+				reject(error)
+			} else {
+				resolve(data)
+			}
+		})
+	})
+
+	return promise
 }
 
-module.exports = { readFilePromise };
+module.exports = { readFilePromise }
